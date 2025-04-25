d66e3716743606a62ca4a57b41ffa6caeb150627
Rohit Kumre <kumrerohit7@gmail.com>
2025-04-25 11:39:24 +0530

    remove no. of item heading

--- src/Pages/TodoApp.jsx
+++ src/Pages/TodoApp.jsx
@@ -3,13 +3,11 @@ import React,{useState,useEffect} from 'react'
 const TodoApp = () => {
     const [list,setList] = useState([])
     const [item,setItem] = useState('')
-    const [count,setCount] = useState(0)
     const myTaskAdd =()=>{
         if(item!=""){
             list.push(item)
             setList(list)
             setItem("")
-            setCount(count+1)
         }
         else{
             alert("Please enter your task")
@@ -21,7 +19,7 @@ const TodoApp = () => {
             setList(listUpdate)
     }
     const editTask = ()=>{
-
+         alert("this is a edit task")
     }
     useEffect(()=>{
       console.log("apka program chall raha hai")
@@ -30,8 +28,8 @@ const TodoApp = () => {
     <div className='container'>
         <h1>Todo App</h1>
           <input type="text" value={item} placeholder='enter your task' onChange={(e)=>setItem(e.target.value)} />
-          <button onClick={myTaskAdd}>Add</button>
-          <h2>No. of Task {count}</h2>
+          <button onClick={myTaskAdd}>Add</button> <br /><br />
+
           <table border={1} className='task-table'>
             <tr>
                 <th>S.No</th>
@@ -46,7 +44,7 @@ const TodoApp = () => {
                     <td>{index+1}</td>
                     <td>{elem}</td>
                     <td><button onClick={()=>removeTask(index)}>Remove</button></td>
-                    <td><button>Edit Task</button></td>
+                    <td><button onClich={()=>editTask()}>Edit Task</button></td>
                    </tr>
               </>   
             )
